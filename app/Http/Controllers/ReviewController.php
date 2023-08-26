0ce6af32d51d08c7c05631cb83bec54e9397034e
ZACKtzyyy <mzhakim22003@gmail.com>
2023-08-26 08:12:29 +0800

    updated_avg

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -31,63 +31,64 @@ class ReviewController extends Controller
             return response()->json(["success" => false, "message" => $err->getMessage()]);
         }
     }
-
     public function update(Request $request, $placeId, $reviewId)
-{
-    try {
-        $user = JWTAuth::parseToken()->authenticate();
-        $userId = $user->id;
-
-        $review = Review::where('user_id', $userId)
-            ->where('place_id', $placeId)
-            ->where('id', $reviewId)
-            ->first();
-
-        if (!$review) {
-            return response()->json(["success" => false, "message" => "Review not found or you are not authorized"]);
+    {
+        try {
+            $user = JWTAuth::parseToken()->authenticate();
+            $userId = $user->id;
+    
+            $review = Review::where('user_id', $userId)
+                ->where('place_id', $placeId)
+                ->where('id', $reviewId)
+                ->first();
+    
+            if (!$review) {
+                return response()->json(["success" => false, "message" => "Review not found or you are not authorized"]);
+            }
+    
+          
+            $ratingDifference = $request->rating - $review->rating;
+    
+            $review->rating = $request->rating;
+            $review->comments = $request->comments;
+            $review->save();
+    
+            $place = Place::find($placeId);
+            $place->avg_rating = ($place->avg_rating * count($place->reviews) + $ratingDifference) / count($place->reviews);
+            $place->save();
+    
+            return response()->json(["success" => true, "message" => "Review successfully updated"]);
+        } catch (\Exception $err) {
+            return response()->json(["success" => false, "message" => $err->getMessage()]);
         }
-
-        $review->rating = $request->rating;
-        $review->comments = $request->comments;
-        $review->save();
-
-        return response()->json(["success" => true, "message" => "Review successfully updated"]);
-    } catch (\Exception $err) {
-        return response()->json(["success" => false, "message" => $err->getMessage()]);
     }
-}
-
-
+    
     public function delete($placeId, $reviewId)
     {
         try {
             $user = JWTAuth::parseToken()->authenticate();
             $userId = $user->id;
-
+    
             $review = Review::where('user_id', $userId)
                 ->where('place_id', $placeId)
                 ->where('id', $reviewId)
                 ->first();
-
+    
             if (!$review) {
                 return response()->json(["success" => false, "message" => "Review not found or you are not authorized"]);
             }
-
+    
+            $ratingDifference = -$review->rating;
+    
             $review->delete();
-
-            // Recalculate average rating for the place
+    
             $place = Place::find($placeId);
-            $reviews = $place->reviews;
-            $totalRating = 0;
-            foreach ($reviews as $review) {
-                $totalRating += $review->rating;
-            }
-            $place->avg_rating = count($reviews) > 0 ? $totalRating / count($reviews) : 0;
+            $place->avg_rating = ($place->avg_rating * count($place->reviews) + $ratingDifference) / count($place->reviews);
             $place->save();
-
+    
             return response()->json(["success" => true, "message" => "Review successfully deleted"]);
         } catch (\Exception $err) {
             return response()->json(["success" => false, "message" => $err->getMessage()]);
         }
     }
-}
+}    
